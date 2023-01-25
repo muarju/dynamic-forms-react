c25cf08493f9ae4ba1a2269c03192a21c9646357
Muslim Uddin Arju <mdarju240@gmail.com>
2023-01-25 16:49:13 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
+  const [formFields, setFormFields] = useState([
+    { type: '', value: '' }
+  ]);
 
-    </div>
+  const handleChange = (event, index) => {
+    let data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  }
+
+  const addField = () => {
+    let object = { type: '', value: '' }
+    setFormFields([...formFields, object])
+  }
+
+  const removeField = (index) => {
+    const updatedFields = [...formFields];
+    updatedFields.splice(index, 1);
+    setFormFields(updatedFields);
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formFields)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Dynamic Forms in React</h3>
+      {formFields.map((field, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            name="type"
+            placeholder="Type Type"
+            style={{ margin: "5px" }}
+            onChange={event => handleChange(event, index)}
+            value={field.type}
+          />
+          <input
+            type="text"
+            name="value"
+            placeholder="Type value"
+            style={{ margin: "5px" }}
+            onChange={event => handleChange(event, index)}
+            value={field.value}
+          />
+          <button type="button" onClick={() => removeField(index)}>Remove</button>
+        </div>
+      ))}
+      <button type="button" onClick={addField}>Add Field</button>
+      <hr />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
